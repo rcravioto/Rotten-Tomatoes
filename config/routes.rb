b8494e3355e8dbcbdb6d5a1195bf4601d68f82fe
rene <r@c.com>
2014-06-04 03:33:44 +0000

    adding links to log in, sign up, or log out

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ RottenMangoes::Application.routes.draw do
 
   resources :users, only: [:new, :create]
   
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
